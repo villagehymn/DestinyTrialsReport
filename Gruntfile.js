0067255a315999b6fd105d26a9fb69faed747ac7
Steffan Long <Steffwiz@me.com>
2015-07-17 03:04:20 -0700

    fix fontawesome fonts not copying to dist, removed unused library

--- Gruntfile.js
+++ Gruntfile.js
@@ -575,9 +575,9 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>'
         }, { 
           expand: true, 
-          cwd: '<%= yeoman.app %>/bower_components/font-awesome', 
-          src: 'fonts/*', 
-          dest: '<%= yeoman.dist %>' 
+          cwd: 'bower_components/font-awesome/fonts', 
+          src: '*',
+          dest: '<%= yeoman.dist %>/fonts'
         }]
       },
       styles: {
