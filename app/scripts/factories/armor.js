aac23beeb6ba1639fc7a03518ec4ce293e8376c0
v-fedorov <vfedorov@netflexity.com>
2015-07-22 11:20:04 -0400

    buncha refactors, and added toastr for error messages

--- app/scripts/factories/armor.js
+++ app/scripts/factories/armor.js
@@ -3,46 +3,55 @@
 angular.module('trialsReportApp')
   .factory('armorStats', function($http) {
     var getData = function(items) {
-      //return $http({method:'GET', url: '/json/armor.json'}).then(function(data){
-        var armors = [];
-        armors.hazards = [];
-        var hazardPerks = ['Light Beyond Nemesis', 'Crest of Alpha Lupi'];
-        var grenadePerks = ['Lucky Raspberry', 'Voidfang Vestments'];
-        var doubleNade = ['The Armamentarium'];
-        //144602215 1735777505 4244567218
-        var intellect = 0;
-        var discipline = 0;
-        var strength = 0;
-        angular.forEach(items,function(item){
-          var itemS = item.items[0];
-          //var aItem = data.data.items[itemS.itemHash];
-          var aItem = armorDefinitions[itemS.itemHash];
-          if (aItem) {
-            if (hazardPerks.indexOf(aItem.name) > -1) {
-              armors.hazards.push('Quick Revive');
-            }else if (grenadePerks.indexOf(aItem.name) > -1){
-              armors.hazards.push('Grenade on Spawn');
-            }else if (doubleNade.indexOf(aItem.name) > -1){
-              armors.hazards.push('Double Grenade');
-            }
-            angular.forEach(itemS.stats,function(stat){
-              switch(stat.statHash) {
-                case 144602215:
-                  intellect += stat.value;
-                  break;
-                case 1735777505:
-                  discipline += stat.value;
-                  break;
-                case 4244567218:
-                  strength += stat.value;
-                  break;
-              }
-            });
-            armors[aItem.bucket] = aItem;
+      var armors = [];
+      armors.hazards = [];
+      var hazardPerks = ['Light Beyond Nemesis', 'Crest of Alpha Lupi'];
+      var grenadePerks = ['Lucky Raspberry', 'Voidfang Vestments'];
+      var doubleNade = ['The Armamentarium'];
+      var intellect = 0;
+      var discipline = 0;
+      var strength = 0;
+      angular.forEach(items,function(item){
+        var itemS = item.items[0];
+        var aItem = armorDefinitions[itemS.itemHash];
+        if (aItem) {
+          if (hazardPerks.indexOf(aItem.name) > -1) {
+            armors.hazards.push('Quick Revive');
+          }else if (grenadePerks.indexOf(aItem.name) > -1){
+            armors.hazards.push('Grenade on Spawn');
+          }else if (doubleNade.indexOf(aItem.name) > -1){
+            armors.hazards.push('Double Grenade');
           }
-        });
-        return {armors: armors, int: intellect, dis: discipline, str: strength};
-      //});
+          angular.forEach(itemS.stats,function(stat){
+            switch(stat.statHash) {
+              case 144602215:
+                intellect += stat.value;
+                break;
+              case 1735777505:
+                discipline += stat.value;
+                break;
+              case 4244567218:
+                strength += stat.value;
+                break;
+            }
+          });
+          switch (aItem.bucket) {
+            case 'BUCKET_HEAD':
+              armors.head = {'armor': aItem};
+              break;
+            case 'BUCKET_ARMS':
+              armors.arms = {'armor': aItem};
+              break;
+            case 'BUCKET_CHEST':
+              armors.chest = {'armor': aItem};
+              break;
+            case 'BUCKET_LEGS':
+              armors.legs = {'armor': aItem};
+              break;
+          }
+        }
+      });
+      return {armors: armors, int: intellect, dis: discipline, str: strength};
     };
     return { getData: getData };
   });
--- app/scripts/factories/weapon.js
+++ app/scripts/factories/weapon.js
@@ -3,80 +3,77 @@
 angular.module('trialsReportApp')
   .factory('weaponStats', function($http) {
     var getData = function(items, talentGrid) {
-      //return $http({method: 'GET', url: '/json/weapons.json'}).then(function (data) {
-        var avoidNodes = ['Ascend', 'Reforge Ready', 'Void Damage', 'Arc Damage', 'Solar Damage', 'Kinetic Damage',
-          'Hive Disruptor', 'Oracle Disruptor', 'Wolfpack Rounds', 'Last Word', 'Fan Fire', 'Mark of the Devourer',
-          'Ice Breaker', 'No Backpack', 'Lich Bane', 'Invective', 'Cursebringer', 'Disciplinarian', 'Holding Aces',
-          'Timeless Mythoclast', 'Thunderer'];
-        var burns = ['Void Damage', 'Arc Damage', 'Solar Damage'];
-        var shotgun = false;
-        var weapons = [];
-        weapons.primary = [];
-        weapons.special = [];
-        weapons.heavy = [];
-        weapons.hazards = [];
-        angular.forEach(items, function (item) {
-          var nodes = [];
-          var itemS = item.items[0];
-          //var wItem = data.data.items[itemS.itemHash];
-          var wItem = weaponDefinitions[itemS.itemHash];
-          if (wItem) {
-            if (wItem.subType === 'Sniper Rifle') {
-              angular.forEach(itemS.stats,function(stat){
-                if (stat.statHash === 4043523819 && stat.value > 16){
-                  if ((itemS.primaryStat.value * stat.value) > 8577){
-                    weapons.hazards.push('Revive Kill Sniper');
-                  }
-                }
-              });
-            } else if (wItem.subType === 'Shotgun') {
-              shotgun = true;
-            }
-            angular.forEach(itemS.nodes, function (node, index) {
-              if (node.isActivated === true) {
-                var nodeStep = talentGrid[itemS.talentGridHash].nodes[index].steps[node.stepIndex];
-                if (!nodeStep.affectsQuality && (avoidNodes.indexOf(nodeStep.nodeStepName) < 0)) {
-                  var longNames = ['Grenades and Horseshoes'];
-                  var name = (longNames.indexOf(nodeStep.nodeStepName) > -1) ? 'Nades & Shoes' : nodeStep.nodeStepName;
-                  if (wItem.subType === 'Sniper Rifle') {
-                    if (nodeStep.perkHashes[0] === 3752206822) {
-                      weapons.hazards.push('Final Round Sniper');
-                    }
-                  }
-                  nodes.push({
-                    'name': name, 'description': nodeStep.nodeStepDescription,
-                    'icon': 'http://www.bungie.net' + nodeStep.icon
-                  });
-                }else if (burns.indexOf(nodeStep.nodeStepName) > -1) {
-                  switch (nodeStep.nodeStepName) {
-                    case 'Solar Damage':
-                      wItem.burnColor = 'solar-dmg';
-                      break;
-                    case 'Void Damage':
-                      wItem.burnColor = 'void-dmg';
-                      break;
-                    case 'Arc Damage':
-                      wItem.burnColor = 'arc-dmg';
-                      break;
-                  }
+      var avoidNodes = ['Ascend', 'Reforge Ready', 'Void Damage', 'Arc Damage', 'Solar Damage', 'Kinetic Damage',
+        'Hive Disruptor', 'Oracle Disruptor', 'Wolfpack Rounds', 'Last Word', 'Fan Fire', 'Mark of the Devourer',
+        'Ice Breaker', 'No Backpack', 'Lich Bane', 'Invective', 'Cursebringer', 'Disciplinarian', 'Holding Aces',
+        'Timeless Mythoclast', 'Thunderer'];
+      var burns = ['Void Damage', 'Arc Damage', 'Solar Damage'];
+      var shotgun = false;
+      var weapons = [];
+      weapons.primary = [];
+      weapons.special = [];
+      weapons.heavy = [];
+      weapons.hazards = [];
+      angular.forEach(items, function (item) {
+        var nodes = [];
+        var itemS = item.items[0];
+        var wItem = weaponDefinitions[itemS.itemHash];
+        if (wItem) {
+          if (wItem.subType === 'Sniper Rifle') {
+            angular.forEach(itemS.stats,function(stat){
+              if (stat.statHash === 4043523819 && stat.value > 16){
+                if ((itemS.primaryStat.value * stat.value) > 8577){
+                  weapons.hazards.push('Revive Kill Sniper');
                 }
               }
             });
-            switch (wItem.bucket) {
-              case 'BUCKET_PRIMARY_WEAPON':
-                weapons.primary = {'weapon': wItem, 'nodes': nodes};
-                break;
-              case 'BUCKET_SPECIAL_WEAPON':
-                weapons.special = {'weapon': wItem, 'nodes': nodes};
-                break;
-              case 'BUCKET_HEAVY_WEAPON':
-                weapons.heavy = {'weapon': wItem, 'nodes': nodes};
-                break;
-            }
+          } else if (wItem.subType === 'Shotgun') {
+            shotgun = true;
           }
-        });
-        return {weapons: weapons, shotgun: shotgun};
-      //});
+          angular.forEach(itemS.nodes, function (node, index) {
+            if (node.isActivated === true) {
+              var nodeStep = talentGrid[itemS.talentGridHash].nodes[index].steps[node.stepIndex];
+              if (!nodeStep.affectsQuality && (avoidNodes.indexOf(nodeStep.nodeStepName) < 0)) {
+                var longNames = ['Grenades and Horseshoes'];
+                var name = (longNames.indexOf(nodeStep.nodeStepName) > -1) ? 'Nades & Shoes' : nodeStep.nodeStepName;
+                if (wItem.subType === 'Sniper Rifle') {
+                  if (nodeStep.perkHashes[0] === 3752206822) {
+                    weapons.hazards.push('Final Round Sniper');
+                  }
+                }
+                nodes.push({
+                  'name': name, 'description': nodeStep.nodeStepDescription,
+                  'icon': 'http://www.bungie.net' + nodeStep.icon
+                });
+              }else if (burns.indexOf(nodeStep.nodeStepName) > -1) {
+                switch (nodeStep.nodeStepName) {
+                  case 'Solar Damage':
+                    wItem.burnColor = 'solar-dmg';
+                    break;
+                  case 'Void Damage':
+                    wItem.burnColor = 'void-dmg';
+                    break;
+                  case 'Arc Damage':
+                    wItem.burnColor = 'arc-dmg';
+                    break;
+                }
+              }
+            }
+          });
+          switch (wItem.bucket) {
+            case 'BUCKET_PRIMARY_WEAPON':
+              weapons.primary = {'weapon': wItem, 'nodes': nodes};
+              break;
+            case 'BUCKET_SPECIAL_WEAPON':
+              weapons.special = {'weapon': wItem, 'nodes': nodes};
+              break;
+            case 'BUCKET_HEAVY_WEAPON':
+              weapons.heavy = {'weapon': wItem, 'nodes': nodes};
+              break;
+          }
+        }
+      });
+      return {weapons: weapons, shotgun: shotgun};
     };
     return { getData: getData };
   });
