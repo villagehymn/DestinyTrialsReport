e4ebb6a365571a3123464cfcb4f5ee970830269a
Steffan Long <Steffwiz@me.com>
2015-07-26 18:16:13 -0700

    further proof we're terrible with decimals

--- app/scripts/factories/inventory.js
+++ app/scripts/factories/inventory.js
@@ -44,24 +44,32 @@ angular.module('trialsReportApp')
               $scope.fireteam[index].cInt = armors.int > 270 ? 270 : armors.int;
               $scope.fireteam[index].cDis = armors.dis > 270 ? 270 : armors.dis;
               $scope.fireteam[index].cStr = armors.str > 270 ? 270 : armors.str;
-              $scope.fireteam[index].intPercent = Math.round(($scope.fireteam[index].cInt / 270) * 100);
-              $scope.fireteam[index].disPercent = Math.round(($scope.fireteam[index].cDis / 270) * 100);
-              $scope.fireteam[index].strPercent = Math.round(($scope.fireteam[index].cStr / 270) * 100);
+              $scope.fireteam[index].intPercent = (100 * $scope.fireteam[index].cInt / 270).toFixed();
+              $scope.fireteam[index].disPercent = (100 * $scope.fireteam[index].cDis / 270).toFixed();
+              $scope.fireteam[index].strPercent = (100 * $scope.fireteam[index].cStr / 270).toFixed();
               $scope.fireteam[index].cTotal = $scope.fireteam[index].cInt + $scope.fireteam[index].cDis + $scope.fireteam[index].cStr;
-              $scope.fireteam[index].cIntPercent = Math.round(($scope.fireteam[index].cInt / $scope.fireteam[index].cTotal) * 100.00);
-              $scope.fireteam[index].cDisPercent = Math.round(($scope.fireteam[index].cDis / $scope.fireteam[index].cTotal) * 100.00);
-              $scope.fireteam[index].cStrPercent = Math.round(($scope.fireteam[index].cStr / $scope.fireteam[index].cTotal) * 100.00);
+              $scope.fireteam[index].cIntPercent = +(100 * $scope.fireteam[index].cInt / $scope.fireteam[index].cTotal).toFixed(2);
+              $scope.fireteam[index].cDisPercent = +(100 * $scope.fireteam[index].cDis / $scope.fireteam[index].cTotal).toFixed(2);
+              $scope.fireteam[index].cStrPercent = +(100 * $scope.fireteam[index].cStr / $scope.fireteam[index].cTotal).toFixed(2);
 
-              if (($scope.fireteam[index].cIntPercent +
-                   $scope.fireteam[index].cDisPercent +
-                   $scope.fireteam[index].cStrPercent) > 100)
-              {
+              var combinedPercentage = $scope.fireteam[index].cIntPercent + $scope.fireteam[index].cDisPercent + $scope.fireteam[index].cStrPercent;
+              if (combinedPercentage > 100) {
+                var difference = combinedPercentage - 100;
                 if ($scope.fireteam[index].cIntPercent > 0) {
-                  $scope.fireteam[index].cIntPercent--;
-                } else if ($scope.fireteam[index].cDisPercent > 0){
-                  $scope.fireteam[index].cDisPercent--;
+                  $scope.fireteam[index].cIntPercent -= difference;
+                } else if ($scope.fireteam[index].cDisPercent > 0) {
+                  $scope.fireteam[index].cDisPercent -= difference;
                 } else {
-                  $scope.fireteam[index].cStrPercent--;
+                  $scope.fireteam[index].cStrPercent -= difference;
+                }
+              } else if (combinedPercentage < 100) {
+                var difference = 100 - combinedPercentage;
+                if ($scope.fireteam[index].cIntPercent > 0) {
+                  $scope.fireteam[index].cIntPercent += difference;
+                } else if ($scope.fireteam[index].cDisPercent > 0) {
+                  $scope.fireteam[index].cDisPercent += difference;
+                } else {
+                  $scope.fireteam[index].cStrPercent += difference;
                 }
               }
 
