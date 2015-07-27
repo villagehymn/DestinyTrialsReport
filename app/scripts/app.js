025bae5cd868e454e0fd171b5eba3ab61170864f
v-fedorov <vfedorov@netflexity.com>
2015-07-27 19:21:26 -0400

    fix #55 and fix #54

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -89,20 +89,57 @@ function getAllFromParams(currentAccount, $route) {
 function setUser(currentAccount, name, platform) {
   return currentAccount.getAccount(name, platform)
     .then(function (player) {
-    //  if (!angular.isObject(player)) {
-    //    $interval(function () {
-    //      $scope.helpOverlay = true;
-    //    }, 1000);
-    //  }
       return player;
     });
 }
 
-function getDeej(currentAccount) {
-  return currentAccount.getAccount("Deej BNG", 1)
-    .then(function (player) {
-      player.isDeej = true;
-      return player;
-    });
+function getDeej() {
+  //    Method for outputting log as stringified json
+  //    var cache = [];
+  //    console.log(JSON.stringify(player, function(key, value) {
+  //      if (typeof value === 'object' && value !== null) {
+  //        if (cache.indexOf(value) !== -1) {
+  //          // Circular reference found, discard key
+  //          return;
+  //        }
+  //        // Store value in our collection
+  //        cache.push(value);
+  //      }
+  //      return value;
+  //    }));
+  //    cache = null;
+  return {
+    "id": "4611686018429501017",
+    "name": "DeeJ BNG",
+    "membershipId": "4611686018429501017",
+    "membershipType": 1,
+    "characterId": "2305843009292996472",
+    "className": "Warlock",
+    "classType": 2,
+    "otherCharacters": [{
+      "id": "4611686018429501017",
+      "name": "DeeJ BNG",
+      "classType": 2,
+      "className": "Warlock",
+      "membershipId": "4611686018429501017",
+      "membershipType": 1,
+      "characterId": "2305843009292996472",
+      "level": 32,
+      "int": 67,
+      "dis": 194,
+      "str": 245,
+      "grimoire": 2685,
+      "background": ["http://bungie.net/common/destiny_content/icons/0fc2957b437530a6fec4b241257089bd.jpg"],
+      "emblem": "http://bungie.net/common/destiny_content/icons/ca7a9bb4a45b7d33a4848577a67cfa33.jpg"
+    }],
+    "level": 32,
+    "int": 67,
+    "dis": 194,
+    "str": 245,
+    "grimoire": 2685,
+    "background": ["http://bungie.net/common/destiny_content/icons/0fc2957b437530a6fec4b241257089bd.jpg"],
+    "emblem": "http://bungie.net/common/destiny_content/icons/ca7a9bb4a45b7d33a4848577a67cfa33.jpg",
+    "isDeej": true
+  }
 }
 
