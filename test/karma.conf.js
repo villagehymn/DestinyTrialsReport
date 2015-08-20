f6c2b4708734292de7d97f090645f2f059394ba5
v-fedorov <vfedorov@netflexity.com>
2015-08-20 19:53:44 -0400

    replace cookies with local storage

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -44,7 +44,6 @@ module.exports = function(config) {
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-carousel/dist/angular-carousel.js',
-      'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-loading-bar/build/loading-bar.js',
       'bower_components/moment/moment.js',
       'bower_components/angular-moment/angular-moment.js',
@@ -57,6 +56,7 @@ module.exports = function(config) {
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/bootstrap-material-design/dist/js/material.js',
       'bower_components/bootstrap-material-design/dist/js/ripples.js',
+      'bower_components/ngstorage/ngStorage.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/app.js',
