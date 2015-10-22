9d1919205d584e3d70fed7a17f3055cdea4ad269
Mark Montvai <mark.montvai@gmail.com>
2015-10-22 20:18:49 +0200

    Restyled FAB button, some performance improvements hopefully

---
+++ app/scripts/directives/mobile-click.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('trialsReportApp')
+  .directive("ngMobileClick", [function () {
+    return function (scope, elem, attrs) {
+      elem.bind("touchstart click", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        scope.$apply(attrs["ngMobileClick"]);
+      });
+    }
+  }]);
