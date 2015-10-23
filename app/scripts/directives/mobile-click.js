cd610bbb73ee2dc105573df7fccc02f32b2edc1b
Steffan Long <Steffwiz@me.com>
2015-10-22 19:04:41 -0700

    minor changes

--- app/scripts/directives/mobile-click.js
+++ app/scripts/directives/mobile-click.js
@@ -1,13 +1,12 @@
 'use strict';
 
 angular.module('trialsReportApp')
-  .directive("ngMobileClick", [function () {
-    return function (scope, elem, attrs) {
-      elem.bind("touchstart click", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-
-        scope.$apply(attrs["ngMobileClick"]);
+  .directive('ngMobileClick', [function () {
+    return function (scope, element, attributes) {
+      element.bind('touchstart click', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        scope.$apply(attributes['ngMobileClick']);
       });
-    }
+    };
   }]);
