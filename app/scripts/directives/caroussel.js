d870cdacb21384361ae13c0b741d80190b20be67
Mark Montvai <mark.montvai@gmail.com>
2015-10-25 09:28:02 +0100

    Smooth finger following

--- app/scripts/directives/caroussel.js
+++ app/scripts/directives/caroussel.js
@@ -7,24 +7,25 @@ angular.module('trialsReportApp')
       link: function(scope, ele, attrs, ctrl) {
         var startX;
         var startTransformX;
+        var container = $('.players').first();
         $swipe.bind(ele, {
           start: function (coords) {
-            $('.players').css('transition', 'none');
-            $('.players').css('-webkit-transition', 'none');
+            container.css('transition', 'none');
+            container.css('-webkit-transition', 'none');
             startX = coords.x;
-            startTransformX = parseFloat($('.players').css('transform').split(',')[4]);
+            startTransformX = parseFloat(container.css('transform').split(',')[4]);
             if (isNaN(startTransformX)) {
               startTransformX = 0;
             }
           },
           move: function(coords) {
             var delta = coords.x - startX;
-            $('.players').css('transform', 'translateX(' + (startTransformX + (delta / 3)) + 'px)');
-            $('.players').css('-webkit-transform', 'translateX(' + (startTransformX + (delta / 3)) + 'px)');
-            $('.players').css('-ms-transform', 'translateX(' + (startTransformX + (delta / 3)) + 'px)');
+            container.css('transform', 'translateX(' + (startTransformX + (delta / 2)) + 'px)');
+            container.css('-webkit-transform', 'translateX(' + (startTransformX + (delta / 2)) + 'px)');
+            container.css('-ms-transform', 'translateX(' + (startTransformX + (delta / 2)) + 'px)');
           },
           end: function(coords) {
-            $('.players').removeAttr('style');
+            container.removeAttr('style');
           }
         })
       }
