4123a75ec5c74af5a5db6b55a35acbb0d6db6c64
v-fedorov <vfedorov@netflexity.com>
2015-10-29 06:10:27 -0400

    minor fixes

--- app/scripts/directives/xpull.js
+++ app/scripts/directives/xpull.js
@@ -4,6 +4,7 @@ angular.module('trialsReportApp')
   .directive("ngXpull", function() {
     return function(scope, elm, attr) {
       return $(elm[0]).xpull({
+        'pullThreshold': 100,
         'callback': function() {
           return scope.$apply(attr.ngXpull);
         }
